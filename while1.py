d20abbc6f20606d1ede39134159b74446d0c01b5
zairong <justiceman3211@hotmail.com>
2019-11-23 11:26:07 +0800

    while part

---
+++ while1.py
@@ -0,0 +1,11 @@
+while True:
+	mode = input('起輸入遊戲模式')
+	if mode == 'q':
+	    break
+	elif mode =='1':
+	    print('啟動模式一')
+	elif mode =='2':
+	    print('啟動模式二')
+	else:
+	    print('只能輸入q/1/2') 
+
